9e45826eb6bf1d30ca405a2138575a913adff3a7
Faseel <faseelchemmadan@topas.tech>
2025-05-14 13:13:49 +0200

    jazzy support

--- myahrs_ros2_driver/launch/myahrs_ros2_driver.launch.py
+++ myahrs_ros2_driver/launch/myahrs_ros2_driver.launch.py
@@ -16,9 +16,6 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
@@ -26,27 +23,6 @@ def generate_launch_description():
 
     config_dir = get_package_share_directory('myahrs_ros2_driver')
     config_file = os.path.join(config_dir, 'config', 'config.yaml')
-
-    rviz_config_file = LaunchConfiguration('rviz_config_file')
-    use_rviz = LaunchConfiguration('use_rviz')
-
-    declare_rviz_config_file_cmd = DeclareLaunchArgument(
-        'rviz_config_file',
-        default_value=os.path.join(config_dir, 'rviz', 'imu_test.rviz'),
-        description='Full path to the RVIZ config file to use')
-
-    declare_use_rviz_cmd = DeclareLaunchArgument(
-        'use_rviz',
-        default_value='True',
-        description='Whether to start RVIZ')
-
-    rviz_cmd = Node(
-        condition=IfCondition(use_rviz),
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        arguments=['-d', rviz_config_file],
-        output='screen')
 
     return LaunchDescription([
         Node(
@@ -56,8 +32,5 @@ def generate_launch_description():
             output='screen',
             arguments=['/dev/ttyACM0', '115200'],
             parameters=[config_file]
-        ),
-        declare_rviz_config_file_cmd,
-        declare_use_rviz_cmd,
-        rviz_cmd,
+        )
     ])
